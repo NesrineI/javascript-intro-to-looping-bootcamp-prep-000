7644a0c4add91d9b40ae556144b4d5248a64b9cb
Nesrine <nesrinakon@gmail.com>
2017-11-06 08:43:05 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,3 @@
-function forLoop(array) {
-  for(let i=0; i<array.length; i++){
-    array[array.length+i] = `"I am ${i} strange loop${i === 0 ? '' : 's'}."`
-  }
-  return array
-}
 
 function whileLoop(n) {
   while(n>=0){
