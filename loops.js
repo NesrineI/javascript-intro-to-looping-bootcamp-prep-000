f377d7da9cb0e821045dea69a0d6d4eae7d8487a
Nesrine <nesrinakon@gmail.com>
2017-11-06 08:46:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,6 +6,10 @@ function whileLoop(n) {
   return ("done")
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+}
+
 function doWhileLoop(array) {
   do{
     array.length-=1
